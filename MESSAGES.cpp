819a721aff203463ddfb084e0a1d4dc737e7fe0e
Tim Shi <timgshi@gmail.com>
2013-09-09 17:22:35 -0700

    removing messages.cpp

--- MESSAGES.cpp
+++
@@ -1,9 +0,0 @@
-#include "MESSAGES.h"
-
-//const char * MESSAGES_messages[MESSAGES_max_method_length] = {
-//  "I hope you have a wonderful day :).",
-//  "You are the most beautifulest!",
-//  "Today is going to be a great day!",
-//  "Be the #winningnest that you can be.",
-//  "Don't forget to call 714-878-9551 for a pick-me-up!"
-//};
